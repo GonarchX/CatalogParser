952bf1698d7ebe7ba9cfb3e34c6ae8a57551d159
GonarchX <valentinpronin@mail.ru>
2022-06-18 00:57:59 +0300

    Feat: added catalog parser functionality

---
+++ CatalogParser/CatalogParser.cs
@@ -0,0 +1,81 @@
+﻿using AngleSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using static CatalogParser.ProductParser;
+
+namespace CatalogParser
+{
+    public class CatalogParser
+    {
+        const string domainName = "https://www.toy.ru";
+        const string productUrlsSelector = @"div[class*=""h-100 product-card""] a.d-block.img-link.text-center.gtm-click";
+        public static async Task ParseCatalogInCsvAsync(string sitePath, string csvFilePath, string sep)
+        {
+            List<string> productUrlsOnFirstPage = await ExtractAllProductUrlsOnPageAsync(sitePath, 1);
+            if (productUrlsOnFirstPage == null) throw new Exception($"Unavailable to extract products from first page in path: {domainName}{sitePath}!");
+
+            List<string> productUrlsOnCurrentPage = null;
+            int curCatalogPageNum = 2;
+
+            var tasks = new List<Task<ProductInfo>>();
+            //This loop stops when the current and first pages are equal, because the site returns the first page in the catalog when trying to get a non-existent page
+            while (!IsEqualsCatalogPages(productUrlsOnFirstPage, productUrlsOnCurrentPage))
+            {
+                var syncCurCatalogPage = curCatalogPageNum;
+                productUrlsOnCurrentPage = curCatalogPageNum == 2 ? productUrlsOnFirstPage : productUrlsOnCurrentPage;
+
+                for (int i = 0; i < productUrlsOnCurrentPage.Count; i++)
+                {
+                    int syncCurProductPage = i;
+
+                    tasks.Add(Task.Run(async () =>
+                    {
+                        var prodParser = await BuildProductParserAsync(productUrlsOnCurrentPage[syncCurProductPage]);
+                        return prodParser.ParseProductPage();
+
+                        Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}: I'm parse product page with number {syncCurProductPage} on catalog page with number {syncCurCatalogPage - 1}");                       
+                    }));
+                }
+
+                try
+                {
+                    productUrlsOnCurrentPage = await ExtractAllProductUrlsOnPageAsync(sitePath, curCatalogPageNum);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Can't parse catalog page in path: {sitePath}{curCatalogPageNum}!\nReason: {ex}");
+                }
+
+                curCatalogPageNum++;
+            }
+            var result = await Task.WhenAll(tasks);
+            CsvUtils.WriteProductInfoInFile(csvFilePath, result.ToList(), sep);
+        }
+
+        //Getting all product urls on the catalog page
+        private static async Task<List<string>> ExtractAllProductUrlsOnPageAsync(string sitePath, int catalogPageNumber = 1)
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            string urlParams = $"?filterseccode%5B0%5D=transport&PAGEN_5={catalogPageNumber}";
+            string curCatalogPageUrl = $"{domainName}/{sitePath}/{urlParams}";
+            var context = BrowsingContext.New(config);
+            var document = await context.OpenAsync(curCatalogPageUrl);
+            return document.QuerySelectorAll(productUrlsSelector).Select(x => domainName + x.GetAttribute("href")).ToList();
+        }
+
+        private static bool IsEqualsCatalogPages(List<string> firstCatalogPageUrl, List<string> secondCatalogPageUrl)
+        {
+            if (firstCatalogPageUrl == null || secondCatalogPageUrl == null)
+                return false;
+            if (firstCatalogPageUrl.Equals(secondCatalogPageUrl))
+                return true;
+            else if (firstCatalogPageUrl.Count != secondCatalogPageUrl.Count)
+                return false;
+
+            return firstCatalogPageUrl.SequenceEqual(secondCatalogPageUrl);
+        }
+    }
+}
---
+++ CatalogParser/CsvUtils.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using static CatalogParser.ProductParser;
+
+namespace CatalogParser
+{
+    internal class CsvUtils
+    {
+        public static void WriteProductInfoInFile(string filePath, List<ProductInfo> source, string sep)
+        {
+            if (source == null || source.Count == 0) return;
+
+            //Adding column names to a file if it doesn't exist
+            if (!File.Exists(filePath)) File.AppendAllText(filePath, 
+                String.Join(sep ,source[0].GetType().GetFields().Select(x => x.Name)) + Environment.NewLine);
+
+            StringBuilder sb = new StringBuilder(100);
+            foreach (var record in source)
+            {
+                sb.Append(record.ToString(sep));
+                sb.Append(Environment.NewLine);
+            }
+            File.AppendAllText(filePath, sb.ToString(), Encoding.UTF8);
+        }
+        public static Dictionary<string, object> DictionaryFromType(object atype)
+        {
+            if (atype == null) return new Dictionary<string, object>();
+            Type t = atype.GetType();
+            PropertyInfo[] props = t.GetProperties();
+            Dictionary<string, object> dict = new Dictionary<string, object>();
+            foreach (PropertyInfo prp in props)
+            {
+                object value = prp.GetValue(atype, new object[] { });
+                dict.Add(prp.Name, value);
+            }
+            return dict;
+        }
+    }
+}
